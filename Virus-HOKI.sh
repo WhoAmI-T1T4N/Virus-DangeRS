6b932c5780995447e4733ef29249a433de0ff61e
WhoAmI-T1T4N <92716161+WhoAmI-T1T4N@users.noreply.github.com>
2021-11-15 14:19:40 +0700

    Create Virus-HOKI.sh

---
+++ Virus-HOKI.sh
@@ -0,0 +1,67 @@
+green='\033[0;32m'
+greenlight='\033[32;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+black='\033[30;1m'
+yellow='\033[33;1m'
+figlet Virus
+figlet Dangers
+echo $black "××××××××××××××××××××××"
+echo $red   "Author : Mr.T1T4N"
+echo $red   "Feat : Mr.DENVAS"
+echo $red   "YouTube : T1T4N TZY"
+echo $black "××××××××××××××××××××××"
+echo $black "======================"
+echo $red   "1). Virus Random"
+echo $red   "2). Virus APK"
+echo $red   "3). Virus Merusak"
+echo $red   "4). Virus Ngasal"
+echo $red   "5). Link GC"
+echo $red   "00). KELUAR YA"
+echo $black "======================="
+read -p "Pilih Cepat Beb :" santai
+if [ $santai = "1" ]
+then
+figlet Tunggu
+figlet Sobb
+echo "Tunggu..."
+echo "Lagi Loading, Bebb..."
+echo "Nihh : https://www20.zippyshare.com/v/qV80sqbq/file.html"
+fi
+if [ $santai = "2" ]
+figlet Tunggu
+figlet Sobb
+echo "Tunggu..."
+echo "Lagi Loading, Bebb..."
+echo "Nihh : https://www116.zippyshare.com/v/awkYUum1/file.html"
+fi
+if [ $santai = "3" ]
+then
+figlet Tunggu
+figlet Sobb
+echo "Tunggu..."
+echo "Lagi Loading, Bebb..."
+echo "Nihh : https://www112.zippyshare.com/v/ZCHbb8If/file.html"
+fi
+if [ $santai = "4" ]
+then
+figlet Tunggu
+figlet Sobb
+echo "Tunggu..."
+echo "Lagi Loading, Bebb..."
+echo "Nihh : https://www51.zippyshare.com/v/etiBuePK/file.html"
+fi
+if [ $santai = "5" ]
+then
+figlet Link
+figlet GC
+echo "Nihh :
+https://chat.whatsapp.com/JDBv6ZtESkPDijZvarsxEQ"
+echo "Join Buat Wawasan"
+fi
+if [ $santai = "00" ]
+then
+figlet Dadah
+figlet Beban
+exit
+fi
